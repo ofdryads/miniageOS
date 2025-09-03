c4ced5b89e4594ad9c183bc0fb3176760e56fbb2
ofdryads <71109631+ofdryads@users.noreply.github.com>
2025-09-03 17:27:26 -0400

    Reference path over cd, fix looking for output zip, use different way to wait for user input

--- flash-customize.sh
+++ flash-customize.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-script_in_here="$(dirname "$0")" # project folder root
+script_in_here="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 source "$script_in_here/config.sh"
 
 # if not already in this directory, change to the build output directory
-cd "$LINEAGE_ROOT/out/target/product/$CODENAME" || exit 1
+output_folder="$LINEAGE_ROOT/out/target/product/$CODENAME"
 
 echo "Make sure your phone is plugged into the computer, and that USB debugging on this computer has been enabled and authorized (in developer settings)"
 echo ""
@@ -15,7 +15,8 @@ echo "If you see your device listed above, that is good and you can continue"
 echo ""
 echo "If you do not see any device listed, do NOT press Enter yet. Leave this terminal window open, troubleshoot, then come back and when running 'adb devices' in another terminal window shows your device and says 'device', not 'unauthorized'."
 echo ""
-read -rp "Press Enter to continue to flash the new system image and recovery:"
+echo "Press Enter to continue to flash the new system image and recovery:"
+read -r _
 
 adb -d reboot bootloader
 
@@ -25,11 +26,12 @@ fi
 
 fastboot reboot recovery
 
-read -rp "In the phone recovery menu (where you should be now), go to 'Apply Update' -> 'Apply from ADB', then hit enter: "
+echo "In the phone recovery menu (where you should be now - it might take a second), go to 'Apply Update' -> 'Apply from ADB', then hit enter:"
+read -r _
 
 # Look for the zip file just made from build, then sideload it
 TODAY=$(date +"%Y%m%d")
-dumb_build=$(grep -l "$TODAY" -- ./*.zip 2>/dev/null)
+dumb_build=$(ls "$output_folder"/*"$TODAY"*.zip 2>/dev/null)
 
 #TODO - the build might have finished on a day other than when this script is run --> account for that
 
@@ -43,7 +45,8 @@ else
   exit 1
 fi
 
-read -rp "Is the phone rebooted and unlocked now? Hit Enter when it is"
+echo "Is the phone rebooted and unlocked now? Hit Enter when it is."
+read -r _
 
 # Gray phone
 if [[ "$GRAYSCALE" ]]; then
@@ -83,9 +86,10 @@ adb install aurora-store-latest.apk
 
 echo "Update any third party apps you have installed or add any apps you need (e.g. maps, secure messaging, notes, minimalist launcher)"
 echo "Warning: Some apps like Venmo, bank apps, and certain Google apps will not work."
-read -rp "Press Enter when you are done installing/updating what you need: "
+echo "Hit Enter when you are done installing/updating what you need:"
+read -r _
 
-# Clean up
+# Clean up apk
 echo "Deleting the Aurora Store .apk from your computer..."
 rm -f aurora-store-latest.apk
 
@@ -96,6 +100,7 @@ adb uninstall com.aurora.store
 # NOTE: with my edited proprietary-files.txt, com.google.android.as is not part of the system image anyways
 if [[ "$IS_PIXEL" ]]; then
   echo "Disabling some unnecessary Google programs on the phone..."
+  #TODO check that these are installed
   adb shell pm disable-user --user 0 com.google.android.as # Android System Intelligence
   adb shell pm disable-user --user 0 com.google.android.as.oss # Private Compute Services
 fi
@@ -104,7 +109,7 @@ fi
 # install Google's Pixel camera app, where path to apk is provided by user in config.sh
 if [[ "$IS_PIXEL" && "$GOOGLE_PIXEL_CAMERA" ]]; then
   adb shell pm disable-user --user 0 org.lineageos.aperture
-  adb install "$PIXEL_CAMERA_APK" 
+  adb install "$PIXEL_CAMERA_APK" #TODO check for this string not being empty in config
 fi
 
 echo "Phone is dumber now!"
