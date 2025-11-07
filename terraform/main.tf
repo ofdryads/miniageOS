ef9f4f45a7c6e17a41bc644d5712460f05bef423
ofdryads <71109631+ofdryads@users.noreply.github.com>
2025-11-06 20:35:21 -0500

    Skeleton of parts that need adding

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,10 @@
+variable "MY_IP" {
+  description = "Your public IP address for SSH access"
+  type        = string
+  # Default value will be set by the local data source below
+  # Remove the default here and use the local data block below
+}
+
 provider "aws" {
   region = "us-east-1"
 }
@@ -17,7 +24,32 @@ resource "aws_instance" "app_server" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = "c6a.4xlarge" # Big for building
 
+  root_block_device {
+    volume_size = 400 # 400GB storage
+    volume_type = "gp3" # fast SSD type
+    delete_on_termination = true # it will be saved somewhere else before tearing down
+  }
+
   tags = {
     Name = "build-mini"
+  }
+}
+
+resource "aws_security_group" "build_mini_sg" {
+  name        = "miniageos-sg"
+  description = "Allow SSH inbound"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["${MY_IP}/32"]  # restrict SSH to your IP
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
